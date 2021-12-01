6e71ce6b7ce762550943b615ddee3247e30cd2f5
Martin Loesch <martin@martinloesch.net>
2021-12-01 08:53:37 +0100

    Finished day 1: refactored part 1 solution, implemented part 2.

--- AoC2021/Day01/Program.fs
+++ AoC2021/Day01/Program.fs
@@ -6,16 +6,26 @@ let readLines filePath = System.IO.File.ReadLines(filePath)
 let lines = readLines @"data\puzzle.txt" |> Seq.map System.Int32.Parse |> List.ofSeq
 
 
-let detectIncrease a b =
+let detectIncreaseWin1 a b =
     if (b > a) then
         1
     else
         0
 
-let rec comp a =
+let rec part1Windowing a =
     match a with
-    | hal::har::ta -> (detectIncrease hal har) :: comp (har::ta)
-    //| ha::[] -> []
+    | hal::har::ta -> (detectIncreaseWin1 hal har) :: part1Windowing (har::ta)
+    | _ -> []
+
+let detectIncreaseWin3 a b c d =
+    if (b+c+d > a+b+c) then
+        1
+    else
+        0
+
+let rec part2Windowing a =
+    match a with
+    | ha1::ha2::ha3::ha4::ta -> (detectIncreaseWin3 ha1 ha2 ha3 ha4) :: part2Windowing (ha2::ha3::ha4::ta)
     | _ -> []
 
 [<EntryPoint>]
@@ -23,5 +33,6 @@ let main argv =
     printfn "Day 1: Sonar Sweep\n==================\n"
     //printfn "%A" lines 
     //printfn "%A" (comp lines)
-    printfn "Part 1: number of increases = %d" (List.sum (comp lines))
+    printfn "Part 1: number of increases = %d" (List.sum (part1Windowing lines))
+    printfn "Part 2: number of increases = %d" (List.sum (part2Windowing lines))
     0 // return an integer exit code
