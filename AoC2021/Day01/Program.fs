e10a658cee0c4e569c8121dbcf0e53207e0298d9
Martin Loesch <martin@martinloesch.net>
2021-12-01 08:36:48 +0100

    Implemented part 1 of day 1.

--- AoC2021/Day01/Program.fs
+++ AoC2021/Day01/Program.fs
@@ -2,7 +2,26 @@
 
 open System
 
+let readLines filePath = System.IO.File.ReadLines(filePath)
+let lines = readLines @"data\puzzle.txt" |> Seq.map System.Int32.Parse |> List.ofSeq
+
+
+let detectIncrease a b =
+    if (b > a) then
+        1
+    else
+        0
+
+let rec comp a =
+    match a with
+    | hal::har::ta -> (detectIncrease hal har) :: comp (har::ta)
+    //| ha::[] -> []
+    | _ -> []
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    printfn "Day 1: Sonar Sweep\n==================\n"
+    //printfn "%A" lines 
+    //printfn "%A" (comp lines)
+    printfn "Part 1: number of increases = %d" (List.sum (comp lines))
     0 // return an integer exit code
