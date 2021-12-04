d229720634ed2af04c58120412e26c25251c5f42
Martin Loesch <martin@martinloesch.net>
2021-12-04 12:39:06 +0100

    Added project for day 4.

---
+++ AoC2021/Day04/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let readLines filePath = System.IO.File.ReadLines(filePath)
+let lines = readLines @"data\puzzle.txt" |> List.ofSeq
+
+[<EntryPoint>]
+let main argv =
+    printfn "Day 4: Giant Squid\n==================\n"
+    0 // return an integer exit code
