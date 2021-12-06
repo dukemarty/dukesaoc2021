a409d6d09241babb3535f8616f293c4a3c29caa3
Martin Loesch <martin@martinloesch.net>
2021-12-06 10:30:24 +0100

    Solved day 6, part 1.

--- AoC2021/Day06/Program.fs
+++ AoC2021/Day06/Program.fs
@@ -4,14 +4,39 @@ open System
 open AoC.Utils
 
 
-let part1Growth80days =
-    let res = 0
+let initState data =
+    seq { for i in 0 .. 8 -> i }
+    |> Seq.map (fun i -> (data |> Seq.filter (fun n -> n = i) |> Seq.length))
+    |> List.ofSeq
+
+let updateState state =
+    let h::t = state
+    let t16 = List.take 6 t
+    let t7::t8::__ = List.skip 6 t
+    let res = t16 @ [ (h + t7) ] @ [ t8 ] @ [ h ]
+    res
+
+let rec runDays state days =
+    if (days = 0) then
+        state
+    else
+        runDays (updateState state) (days - 1)
+
+let part1Growth80days data =
+    let state = initState data
+    printfn "  Initial state: %A" state
+    let finalState = runDays state 80
+    printfn "  Final state: %A" finalState
+    let res = finalState |> List.sum
     printfn "Final number of fish after 80 days: %d" res
 
 [<EntryPoint>]
 let main argv =
     printfn "Day 6: Lanternfish\n==================\n"
-    let initState = DataInput.commaSeparatedIntList DataInput.Sample
-    printfn "Initial data: %A" (initState |> List.ofSeq)
-    part1Growth80days
+
+    let data =
+        DataInput.commaSeparatedIntList DataInput.Puzzle
+
+    printfn "Initial data: %A" (data |> List.ofSeq)
+    part1Growth80days data
     0 // return an integer exit code
