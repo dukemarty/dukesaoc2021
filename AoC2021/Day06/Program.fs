620240cdba98a2c6735af8d7491a06c12dd57186
Martin Loesch <martin@martinloesch.net>
2021-12-06 09:40:50 +0100

    Added data for day 6.

--- AoC2021/Day06/Program.fs
+++ AoC2021/Day06/Program.fs
@@ -1,8 +1,17 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open AoC.Utils
+
+
+let part1Growth80days =
+    let res = 0
+    printfn "Final number of fish after 80 days: %d" res
 
 [<EntryPoint>]
 let main argv =
     printfn "Day 6: Lanternfish\n==================\n"
+    let initState = DataInput.commaSeparatedIntList DataInput.Sample
+    printfn "Initial data: %A" (initState |> List.ofSeq)
+    part1Growth80days
     0 // return an integer exit code
