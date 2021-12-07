950fa4ebfba611b42b543135e3920cc22fd61d8a
Martin Loesch <martin@martinloesch.net>
2021-12-08 00:22:50 +0100

    Added project for day 8.

---
+++ AoC2021/Day08/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open AoC.Utils
+
+[<EntryPoint>]
+let main argv =
+    printfn "Day 8: XXXXXXXXXX\n==============================\n"
+
+    let data = DataInput.multipleRawLines DataInput.Sample
+    0 // return an integer exit code
