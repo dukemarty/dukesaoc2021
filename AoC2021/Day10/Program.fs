cc74b9282f1dd5479183fed6567cb0c4c22bee39
Martin Loesch <martin@martinloesch.net>
2021-12-10 00:47:58 +0100

    Added project for day 10.

---
+++ AoC2021/Day10/Program.fs
@@ -0,0 +1,8 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Day 10: XXXXXXXXXXXXX\n==================\n"
+    0 // return an integer exit code
