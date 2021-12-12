ae55332fd466c943d9c941f94df614b2bf9cf8b5
Martin Loesch <martin@martinloesch.net>
2021-12-12 10:28:26 +0100

    Added project for day 12.

---
+++ AoC2021/Day12/Program.fs
@@ -0,0 +1,8 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Day 12: Passage Pathing\n=======================\n"
+    0 // return an integer exit code
