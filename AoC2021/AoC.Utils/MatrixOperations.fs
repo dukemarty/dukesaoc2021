04fe3fd63b6c70eaefa86204e5e9cef091b5e316
Martin Loesch <martin@martinloesch.net>
2021-12-15 21:43:57 +0100

    Solved day 15, part 1.

---
+++ AoC2021/AoC.Utils/MatrixOperations.fs
@@ -0,0 +1,41 @@
+﻿namespace AoC.Utils
+
+module MatrixOperations =
+
+    let createValidNeighboursSimple pos width height =
+        let r, c = pos
+
+        let cands =
+            [| (r, c - 1)
+               (r, c + 1)
+               (r - 1, c)
+               (r + 1, c) |]
+
+        cands
+        |> Seq.filter
+            (fun (r, c) ->
+                (r >= 0)
+                && (c >= 0)
+                && (c < width)
+                && (r < height))
+
+    let createNeighboursWithDiagonals pos width height =
+        let r, c = pos
+
+        let cands =
+            [| (r, c - 1)
+               (r - 1, c - 1)
+               (r - 1, c)
+               (r - 1, c + 1)
+               (r, c + 1)
+               (r + 1, c + 1)
+               (r + 1, c)
+               (r + 1, c - 1) |]
+
+        cands
+        |> Seq.filter
+            (fun (r, c) ->
+                (r >= 0)
+                && (c >= 0)
+                && (c < width)
+                && (r < height))
---
+++ AoC2021/Day15/Program.fs
@@ -0,0 +1,49 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open AoC.Utils
+
+
+let part1LowestTotalRisk (data: int array array) =
+    let h, w = data.Length, data.[0].Length
+
+    let dists =
+        seq { for i in 1 .. h -> i }
+        |> Seq.map (fun _ -> Array.create w -1)
+        |> Array.ofSeq
+
+    let updateNeighbours r c =
+        let neighbours =
+            MatrixOperations.createValidNeighboursSimple (r, c) w h
+
+        let baseVal = dists.[r].[c]
+        let mutable res = []
+
+        for (nr, nc) in neighbours do
+            if ((dists.[nr].[nc] < 0)
+                || ((baseVal + data.[nr].[nc]) < dists.[nr].[nc])) then
+                dists.[nr].[nc] <- baseVal + data.[nr].[nc]
+                res <- (nr, nc) :: res
+
+        res
+
+
+    let rec pathFindHelper openPositions =
+        match openPositions with
+        | (xr, xc) :: xs ->
+            let addPos = updateNeighbours xr xc
+            pathFindHelper (xs @ addPos)
+        | [] -> dists
+
+    dists.[0].[0] <- 0
+    let initialOpenPositions = updateNeighbours 0 0
+    ignore (pathFindHelper initialOpenPositions)
+    printfn "Part 1, lowest total risk: %d" dists.[h - 1].[w - 1]
+
+[<EntryPoint>]
+let main argv =
+    printfn "Day 15: Chiton\n==============\n"
+    let data = DataInput.intMatrix DataInput.Puzzle
+    //printfn "Data: %A" data
+    let res = part1LowestTotalRisk data
+    0 // return an integer exit code
