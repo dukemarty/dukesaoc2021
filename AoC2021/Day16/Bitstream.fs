4a19362565b221868ed2772e24d7a8960631a1d7
Martin Loesch <martin@martinloesch.net>
2021-12-16 23:38:06 +0100

    Solved day 16, part 1.

---
+++ AoC2021/Day16/Bitstream.fs
@@ -0,0 +1,90 @@
+﻿module Bitstream
+
+open System
+
+type Bitstream = string
+
+type Version = int
+type TypeId = int
+
+type Packet =
+    | Lit of Version * UInt64
+    | Op of Version * TypeId * (Packet list)
+
+type ParseMode =
+    | SumLength of int
+    | PackagesCount of int
+
+let splitHeader (bits: char array) =
+    let version =
+        Convert.ToInt32(bits.[0..2] |> System.String, 2)
+
+    let typeId =
+        Convert.ToInt32(bits.[3..5] |> System.String, 2)
+
+    let data = bits.[6..]
+    version, typeId, data
+
+let parse4Literal bits =
+    let rec literalParseHelper (bits: char array) =
+        match bits.[0] with
+        | '0' -> (bits.[1..4] |> System.String), 5
+        | '1' ->
+            let remainder, count = literalParseHelper bits.[5..]
+            (bits.[1..4] |> System.String) + remainder, count + 5
+
+    let extractedBits, consumedCount = literalParseHelper bits
+    Convert.ToUInt64(extractedBits, 2), consumedCount
+
+let parseOperationHeader (bits: char array) =
+    match bits.[0] with
+    | '0' ->
+        let totalSubpacketLength =
+            Convert.ToInt32(bits.[1..15] |> System.String, 2)
+
+        SumLength totalSubpacketLength, 16, bits.[16..]
+    | '1' ->
+        let numberOfSubpackets =
+            Convert.ToInt32(bits.[1..11] |> System.String, 2)
+
+        PackagesCount numberOfSubpackets, 12, bits.[12..]
+    | c -> failwithf "Unexpected (impossible) character: %c" c
+
+let parsePackets (bitstream: string) =
+
+    let rec parseNextPacket bits mode =
+        let v, t, d = splitHeader bits
+        let headerConsumedCount = 6
+
+        let p, consumedCount =
+            match t with
+            | 4 ->
+                let literalValue, consumed = parse4Literal d
+                printfn "  Decoded literal: %d" literalValue
+                Lit(v, literalValue), headerConsumedCount + consumed
+            | _ ->
+                let remMode, opHeaderConsumedCount, remBits = parseOperationHeader d
+                match remMode with
+                | SumLength l ->
+                    let ps, consumed = parseNextPacket remBits.[0..(l-1)] remMode
+                    Op (v, t, ps), headerConsumedCount + opHeaderConsumedCount + consumed
+                | PackagesCount n ->
+                    let ps, consumed = parseNextPacket remBits remMode
+                    Op (v, t, ps), headerConsumedCount + opHeaderConsumedCount + consumed
+
+        match mode with
+        | SumLength n when (n - consumedCount) < 7 -> [p], consumedCount
+        | SumLength n ->
+            let nps, addConsumedCount =
+                parseNextPacket bits.[consumedCount..] (SumLength(n - consumedCount))
+
+            p :: nps, consumedCount + addConsumedCount
+        | PackagesCount 1 -> [p], consumedCount
+        | PackagesCount n ->
+            let nps, addConsumedCount =
+                parseNextPacket bits.[consumedCount..] (PackagesCount(n - 1))
+
+            p :: nps, consumedCount + addConsumedCount
+
+    let bits = bitstream.ToCharArray()
+    parseNextPacket bits (PackagesCount 1)
---
+++ AoC2021/Day16/Program.fs
@@ -0,0 +1,36 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open AoC.Utils
+
+let rec sumVersions packets = 
+    let sumHelper packet =
+        match packet with
+        | Bitstream.Lit (v, _) -> v
+        | Bitstream.Op (v, _, ps) -> v + (sumVersions ps)
+
+    match packets with
+    | [p] -> sumHelper p
+    | p::ps -> (sumHelper p) + (sumVersions ps)
+
+let part1VersionsSum bits =
+    let packets, consumedCount = Bitstream.parsePackets bits
+    printfn "  Packets : %A" packets
+    printfn "  Consumed: %d" consumedCount
+    let res = sumVersions packets
+    printfn "Part 1, sum of all versions: %d" res
+
+[<EntryPoint>]
+let main argv =
+    printfn "Day 16: Packet Decoder\n======================\n"
+
+    let bitstream: Bitstream.Bitstream =
+        (DataInput.singleRawLine (DataInput.Puzzle))
+            .ToCharArray()
+        |> Seq.map (fun c -> Convert.ToInt32(string c, 16))
+        |> Seq.map (fun n -> Convert.ToString(n, 2).PadLeft(4, '0'))
+        |> String.concat ""
+
+    printfn "Data: %s" bitstream
+    part1VersionsSum bitstream
+    0 // return an integer exit code
