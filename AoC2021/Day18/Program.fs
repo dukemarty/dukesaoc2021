77191640c5df0d4de0bf1263b0bfd4213eb04a12
Martin Loesch <martin@martinloesch.net>
2021-12-20 00:24:48 +0100

    Solved day 18, part 2.

--- AoC2021/Day18/Program.fs
+++ AoC2021/Day18/Program.fs
@@ -4,10 +4,33 @@ open System
 open AoC.Utils
 
 let part1LongAddition (data: SnailNumber.SnailNumber list) =
-    let resSum = data |> List.reduce (fun l r -> SnailNumber.addSnailNumbers l r)
+    let resSum =
+        data
+        |> List.reduce (fun l r -> SnailNumber.addSnailNumbers l r)
+
     printfn "  Sum: %O" resSum
     let res = SnailNumber.calcMagnitude resSum
     printfn "Part 1, magnitude of sum: %d" res
+
+
+let part2LargestMagnitude (data: SnailNumber.SnailNumber list) =
+    let rec helper remNums largest =
+        match remNums with
+        | [ x ] -> largest
+        | x :: xs ->
+            let cand =
+                xs
+                |> Seq.collect
+                    (fun sn ->
+                        [ SnailNumber.calcMagnitude (SnailNumber.addSnailNumbers x sn)
+                          SnailNumber.calcMagnitude (SnailNumber.addSnailNumbers sn x) ])
+                |> Seq.max
+
+            helper xs (max largest cand)
+
+    let res = helper data 0
+    printfn "Part 2, largest magnitude: %d" res
+
 
 [<EntryPoint>]
 let main argv =
@@ -15,4 +38,5 @@ let main argv =
     let data = DataParser.loadData (DataInput.Puzzle)
     printfn "Data: %A" data
     part1LongAddition data
+    part2LargestMagnitude data
     0 // return an integer exit code
