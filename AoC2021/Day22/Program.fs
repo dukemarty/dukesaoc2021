d3a41ecafb09efcfeaba881007434ffd508c463d
Martin Loesch <martin@martinloesch.net>
2021-12-23 00:55:51 +0100

    Solved day 22, part 1.

---
+++ AoC2021/Day22/Program.fs
@@ -0,0 +1,61 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open AoC.Utils
+
+let part1HasRelevantParts (step: RebootSteps.RebootStep) =
+    ((step.X.From <= 50) && (step.X.To >= -50) || (step.X.From >= 50) && (step.X.To <= -50))
+    && ((step.Y.From <= 50) && (step.Y.To >= -50) || (step.Y.From >= 50) && (step.Y.To <= -50) )
+    && ((step.Z.From <= 50) && (step.Z.To >= -50) || (step.Z.From >= 50) && (step.Z.To <= -50))
+
+let part1FilterForRelevantSection (step: RebootSteps.RebootStep) =
+    let x = { step.X with From=(min step.X.From step.X.To); To=(max step.X.From step.X.To) }
+    let xx = { x with From = (max x.From -50); To = (min x.To 50) }
+    let y = { step.Y with From=(min step.Y.From step.Y.To); To=(max step.Y.From step.Y.To)}
+    let yy = { y with From = (max y.From -50); To = (min y.To 50) }
+    let z = { step.Z with From=(min step.Z.From step.Z.To); To=(max step.Z.From step.Z.To)}
+    let zz = { z with From = (max z.From -50); To = (min z.To 50) }
+
+    { step with X = xx; Y = yy; Z = zz }
+
+let stepToCuboids (step: RebootSteps.RebootStep) =
+    let res =
+        seq { for x in step.X.From .. step.X.To -> x }
+        |> Seq.collect
+            (fun x ->
+                seq { for y in step.Y.From .. step.Y.To -> y }
+                |> Seq.collect
+                    (fun y ->
+                        seq { for z in step.Z.From .. step.Z.To -> z }
+                        |> Seq.map (fun z -> (x, y, z))))
+        |> Set.ofSeq
+    //printfn "  Created cuboids for (%O): %A" step res
+    res
+
+let part1CountTurnedOnCubes data =
+    let applyStep onSet (step: RebootSteps.RebootStep) =
+        let res = match step.Action with
+                  | RebootSteps.On -> Set.union onSet (stepToCuboids step)
+                  | RebootSteps.Off -> Set.difference onSet (stepToCuboids step)
+        printfn "  Set after applying (%d..%d, %d..%d, %d..%d): %d" step.X.From step.X.To step.Y.From step.Y.To step.Z.From step.Z.To res.Count
+        res
+    let focussedData =
+        data
+        |> Seq.filter part1HasRelevantParts
+        |> Seq.map part1FilterForRelevantSection
+
+    printfn "  Focussed data: %A" (focussedData |> List.ofSeq)
+    let onSet = focussedData |> Seq.fold applyStep Set.empty
+    let res = onSet.Count
+    printfn "Part 1, #cubes=on: %d" res
+
+[<EntryPoint>]
+let main argv =
+    printfn "Day 22: Reactor Reboot\n======================\n"
+
+    let data =
+        RebootSteps.loadAllSteps (DataInput.Puzzle)
+
+    //printfn "Data: %A" data
+    part1CountTurnedOnCubes data
+    0 // return an integer exit code
---
+++ AoC2021/Day22/RebootSteps.fs
@@ -0,0 +1,43 @@
+﻿module RebootSteps
+
+open System
+open System.Text.RegularExpressions
+open AoC.Utils
+
+type RebootAction =
+    | On
+    | Off
+
+type Range = { From: int; To: int }
+
+type RebootStep =
+    { Action: RebootAction
+      X: Range
+      Y: Range
+      Z: Range }
+
+
+let parseRebootStep line =
+    let m =
+        Regex.Match(line, "(\w+)\s+x=(-?\d+)\.\.(-?\d+),y=(-?\d+)\.\.(-?\d+),z=(-?\d+)\.\.(-?\d+)")
+
+    let action =
+        match m.Groups.[1].Value with
+        | "on" -> On
+        | "off" -> Off
+        | s -> failwithf "Unknown action found: %s" s
+
+    { Action = action
+      X =
+          { From = (Int32.Parse m.Groups.[2].Value)
+            To = (Int32.Parse m.Groups.[3].Value) }
+      Y =
+          { From = (Int32.Parse m.Groups.[4].Value)
+            To = (Int32.Parse m.Groups.[5].Value) }
+      Z =
+          { From = (Int32.Parse m.Groups.[6].Value)
+            To = (Int32.Parse m.Groups.[7].Value) } }
+
+let loadAllSteps source =
+    let lines = DataInput.multipleRawLines source
+    lines |> List.map parseRebootStep
